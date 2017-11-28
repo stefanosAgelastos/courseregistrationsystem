06410cc47cede67a307594b0d4eae956b83d24f6
Stefanos <sagelastos@gmail.com>
2017-11-28 13:54:06 +0100

    removed tomcat, package in war

--- src/main/java/kea/project/exam/courseregistrationsystem/CourseregistrationsystemApplication.java
+++ src/main/java/kea/project/exam/courseregistrationsystem/CourseregistrationsystemApplication.java
@@ -2,9 +2,16 @@ package kea.project.exam.courseregistrationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseregistrationsystemApplication {
+public class CourseregistrationsystemApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CourseregistrationsystemApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseregistrationsystemApplication.class, args);
